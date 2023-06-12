c083ca33f0c9a93cde8e43bef9e11e464530174a
stefanymarinho <126528055+stefanymarinho@users.noreply.github.com>
2023-06-12 07:55:12 -0300

    primeira versão do site

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// usado para importar as bibliotecas 
+$(document).ready(function(){
+    $("#banners ul").bxSlider({
+        auto: true,
+        mode: 'fade',
+        pager:false
+	
+	
+		
+    });
+  });
+
+  
+
+//////////////////////////////
+
+
+
+
+	//////////////////////////////////////////////////////////////
+
+
